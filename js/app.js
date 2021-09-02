0842fb90dc86712469aa4a283480c81326e92865
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-09-02 15:29:46 +0600

    add result info

--- js/app.js
+++ js/app.js
@@ -35,8 +35,17 @@ const errorMessage = () => {
 // books card declare
 const bookCard = (books) => {
     const booksContainer = document.getElementById('books-container');
+    const booksResults = document.getElementById('resultDiv');
     booksContainer.textContent = '';
+    booksResults.textContent = '';
+
+    // books results
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h5 class="display-6 fs-5 fw-bolder container text-center mt-5">About result found: <span class="text-info fw-bolder">${books.length}</span></h5>`;
+    booksResults.appendChild(div);
         books.forEach(book => {
+            // all books card
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML = `
         <div class="col">
