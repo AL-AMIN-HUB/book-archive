999b627e5e0e3da6b17b444c111b0d0a0a555a3f
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-09-02 11:56:00 +0600

    error message

--- js/app.js
+++ js/app.js
@@ -1,24 +1,43 @@
-
+// search input and button handle hear
 const loadBooks = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
-
-    // clear
-    searchInput.value = '';
     
-    const url = `https://openlibrary.org/search.json?q=${searchText}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => bookCard(data.docs));
+        // clear
+        searchInput.value = '';
+
+        // error handle
+    if (searchText ==='') {
+        errorMessage();
+    }
+
+    else{
+         document.getElementById('error-message').style.display = 'none';
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => bookCard(data.docs));
+    }
     
 }
+// error message
+const errorMessage = () => {
+   const errorMess = document.getElementById('error-message');
+   const div = document.createElement('div');
+   div.innerHTML = `<div class="container rounded-3 shadow mt-5 text-center p-3 bg-dark"> 
+   <h3 class=" fw-bold text-muted"> <span class="text-danger">No results found.</span> Search for books containing the " " phrase?</h3>
+   </div>`;
+   errorMess.appendChild(div);
+}
 
+// book card declare
 const bookCard = (books) => {
     const booksContainer = document.getElementById('books-container');
     booksContainer.textContent = '';
-    books.forEach(book => {
-        const  cardDiv  = document.createElement('div');
-        cardDiv.innerHTML = `
+   
+        books.forEach(book => {
+            const cardDiv = document.createElement('div');
+            cardDiv.innerHTML = `
         <div class="col">
                     <div class="card h-100 border-0 shadow rounded-3">
                     <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top d-block img-fluid" alt="Cover image">
@@ -30,7 +49,7 @@ const bookCard = (books) => {
                     </div>
                 </div>        
         `;
-        booksContainer.appendChild(cardDiv);
-        
-    })
+            booksContainer.appendChild(cardDiv);
+
+        })
 };
